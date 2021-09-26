e86d207e32701f7f48abbbe834dddafe62ac99be
Denis <chilikin.den@gmail.com>
2021-09-26 19:32:30 +0300

    first commit

---
+++ Lesson_1/Class_Work/index.js
@@ -0,0 +1,17 @@
+const planetEarth = "планета Земля";
+let userCurrentTime = "23:00";
+let articleCount = 2;
+let isPaymentCash = true;
+let firstName = "Денис";
+let surname = "Чиликин";
+let middleName = "Игоревич";
+
+console.log(
+  planetEarth,
+  userTime,
+  articleCount,
+  isPaymentCash,
+  firstName,
+  surname,
+  middleName
+);
---
+++ Lesson_1/Home_Work/index.js
@@ -0,0 +1,107 @@
+// console.log("Hellow world");
+
+//===============================================
+
+// let num = 27;
+// let str = "строка";
+// let logics = true;
+// let man = null;
+// let car;
+// let user = {
+//   name: "Денис",
+//   age: 27,
+// };
+// let sym = Symbol();
+// const theBiggestInt = 9007199254740991n;
+
+//===============================================
+
+// console.log(num, str, logics, man, car, user, sym, theBiggestInt);
+
+//===============================================
+
+// console.log(typeof num);
+// console.log(typeof str);
+// console.log(typeof logics);
+// console.log(typeof man);
+// console.log(typeof car);
+// console.log(typeof user);
+// console.log(typeof sym);
+// console.log(typeof theBiggestInt);
+
+//==============================================
+
+// const pi = 3.14;
+// pi = "3.14";
+// console.log(pi);
+
+//==============================================
+
+// const transportCount = {
+//   car: 3,
+//   bus: 12,
+// };
+
+// transportCount = {
+//   car: null,
+// };
+// console.log(transportCount);
+
+// transportCount = {
+//   car: 3,
+//   bus: 12,
+//   bike: 5,
+// };
+// console.log(transportCount);
+
+// transportCount.bike = 5;
+// delete transportCount.car;
+// console.log(transportCount);
+
+//==============================================
+
+// let transportCount = {
+//   car: 3,
+//   bus: 12,
+// };
+
+// transportCount = {
+//   car: null,
+// };
+// console.log(transportCount);
+
+// transportCount = {
+//   car: 3,
+//   bus: 12,
+//   bike: 5,
+// };
+// console.log(transportCount);
+
+// transportCount.bike = 5;
+// delete transportCount.car;
+// console.log(transportCount);
+
+//==============================================
+
+// var transportCount = {
+//   car: 3,
+//   bus: 12,
+// };
+
+// transportCount = {
+//   car: null,
+// };
+// console.log(transportCount);
+
+// transportCount = {
+//   car: 3,
+//   bus: 12,
+//   bike: 5,
+// };
+// console.log(transportCount);
+
+// transportCount.bike = 5;
+// delete transportCount.car;
+// console.log(transportCount);
+
+//==============================================
